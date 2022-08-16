cfaa4a253475d75056e45d22423e764fe5a2c0e8
nikitapryymak <nikitapryymak29@gmail.com>
2022-08-15 22:52:45 -0700

    Remove assertion for leaderboard data length

--- controllers/leaderboard.controller.js
+++ controllers/leaderboard.controller.js
@@ -1,14 +1,8 @@
-const { ENTITY_NOT_FOUND } = require("../constants/AppErrorCodes.js");
 const { getLeaderboard } = require("../services/leaderboard.service.js");
-const { tryCatch, assertCondition } = require("../utils/utils.js");
+const { tryCatch } = require("../utils/utils.js");
 
 exports.getLeaderboardController = tryCatch(async (req, res) => {
   const leaderboard = await getLeaderboard();
-  assertCondition(
-    leaderboard.length > 0,
-    ENTITY_NOT_FOUND,
-    "No leaderboard data found"
-  );
 
   res.json(leaderboard);
 });
