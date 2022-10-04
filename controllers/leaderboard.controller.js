e4b1ec2d959a7e6a42892db7ec1e609e38f42553
nikitapryymak <nikitapryymak29@gmail.com>
2022-10-04 00:22:33 -0700

    Add 'total' row to leaderboard

--- controllers/leaderboard.controller.js
+++ controllers/leaderboard.controller.js
@@ -1,8 +1,42 @@
 const { getLeaderboard } = require("../services/leaderboard.service.js");
-const { tryCatch } = require("../utils/utils.js");
+const { tryCatch, addTotals } = require("../utils/utils.js");
+
+const initialTotals = {
+  playerId: 0,
+  Last: "Total",
+  "1B": 0,
+  "2B": 0,
+  "2OUTRBI": 0,
+  "3B": 0,
+  AB: 0,
+  BB: 0,
+  BPA: 0,
+  CI: 0,
+  FC: 0,
+  H: 0,
+  HBP: 0,
+  HEADSUP: 0,
+  HR: 0,
+  PA: 0,
+  R: 0,
+  RBI: 0,
+  ROE: 0,
+  SAC: 0,
+  SB: 0,
+  SF: 0,
+  SO: 0,
+  TB: 0,
+};
 
 exports.getLeaderboardController = tryCatch(async (req, res) => {
   const leaderboard = await getLeaderboard();
+  const totals = leaderboard.reduce(
+    (total, stat) => addTotals(total, stat),
+    initialTotals
+  );
 
-  return res.json(leaderboard);
+  const totalAverageBPA = totals.BPA / Object.keys(leaderboard).length;
+  const response = [...leaderboard, { ...totals, BPA: totalAverageBPA }];
+
+  return res.json(response);
 });
--- utils/utils.js
+++ utils/utils.js
@@ -18,3 +18,29 @@ exports.validator = (schema, payload) =>
   schema.validate(payload, { abortEarly: false, stripUnknown: true });
 
 exports.verifyAdminKey = (key) => key && key === ADMIN_KEY;
+
+exports.addTotals = (total, stat) => ({
+  ...total,
+  "1B": total["1B"] + parseInt(stat["1B"]),
+  "2B": total["2B"] + parseInt(stat["2B"]),
+  "3B": total["3B"] + parseInt(stat["3B"]),
+  AB: total["AB"] + parseInt(stat["AB"]),
+  BB: total["BB"] + parseInt(stat["BB"]),
+  CI: total["CI"] + parseInt(stat["CI"]),
+  FC: total["FC"] + parseInt(stat["FC"]),
+  H: total["H"] + parseInt(stat["H"]),
+  HBP: total["HBP"] + parseInt(stat["HBP"]),
+  HR: total["HR"] + parseInt(stat["HR"]),
+  PA: total["PA"] + parseInt(stat["PA"]),
+  R: total["R"] + parseInt(stat["R"]),
+  RBI: total["RBI"] + parseInt(stat["RBI"]),
+  ROE: total["ROE"] + parseInt(stat["ROE"]),
+  SAC: total["SAC"] + parseInt(stat["SAC"]),
+  SB: total["SB"] + parseInt(stat["SB"]),
+  SF: total["SF"] + parseInt(stat["SF"]),
+  SO: total["SO"] + parseInt(stat["1B"]),
+  TB: total["TB"] + parseInt(stat["TB"]),
+  "2OUTRBI": total["2OUTRBI"] + parseInt(stat["2OUTRBI"]),
+  HEADSUP: total["HEADSUP"] + parseInt(stat["HEADSUP"]),
+  BPA: total["BPA"] + parseInt(stat["BPA"]),
+});
